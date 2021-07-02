a0a5d23a159960b9f56ff6c72f982237ed126314
achalb-asu <achelb@advsysunlimited.com>
2021-07-02 17:12:05 +0530

    made changes to authapi

--- app/src/main/java/com/example/respitrekdemoapp/network/AuthApi.kt
+++ app/src/main/java/com/example/respitrekdemoapp/network/AuthApi.kt
@@ -1,5 +1,6 @@
 package com.example.respitrekdemoapp.network
 
+import com.example.respitrekdemoapp.responses.LoginResponse
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
@@ -7,8 +8,8 @@ import retrofit2.http.POST
 interface AuthApi {
    @FormUrlEncoded
    @POST("auth/login")
-    fun login(
+   suspend fun login(
         @Field("email") email:String,
         @Field("password") password:String
-    ):Any
+    ):LoginResponse
 }
